2b2b18d6420b4088509b1a776d9f825bbb214ca2
JingeYu <65295370+jingeyu@users.noreply.github.com>
2021-03-10 21:01:42 +0800

    Add files via upload

--- R/CSSN.R
+++ R/CSSN.R
@@ -1,18 +1,21 @@
-CSSNEst <- function(X, cell.info, nu, d = 0.1, m.info = 70, is.scale = TRUE){
+CSSNEst <- function(X, cell.info, nu, d = 0.1,
+                    m.info = 70, is.scale = TRUE,
+                    is.all = TRUE, indx.cell = NULL){
+  library(Matrix)
   #---- Step0 Preprocess of Data ------
   # if there is NaNs in the dataset
   if(sum(is.na(X)) > 0){
     stop("Please remove NaNs in the dataset!!!")
   }
-  
+
   if(d < 0 & d > 1){
     stop("Please set threshold parameter d between 0 and 1!")
   }
-  
+
   if(d < 0 & d > 1){
     stop("Please set threshold parameter d between 0 and 1!")
   }
-  
+
   # Cell number:
   n <- ncol(X)
   # Gene number
@@ -28,7 +31,7 @@ CSSNEst <- function(X, cell.info, nu, d = 0.1, m.info = 70, is.scale = TRUE){
   }
   cell.type <- as.numeric(cell.type)
   ind.cell.type <- list()
-  
+
   #---- Step1 Centralization and Scaling ----
   for(k in 1:K){
     tmp <- which(cell.type == k)
@@ -38,12 +41,12 @@ CSSNEst <- function(X, cell.info, nu, d = 0.1, m.info = 70, is.scale = TRUE){
     }
     ind.cell.type[[k]] <- tmp
   }
-  
+
   NA.num <- sum(is.na(X))
   if(NA.num > 0){
     stop("Please remove genes having zero values across one cell-type!")
   }
-  
+
   cell.info <- as.data.frame(cell.info)
   colnames(cell.info) <- c("CT", "X", "Y")
   #---- Step2 Estimate Sigma_k ----
@@ -52,20 +55,20 @@ CSSNEst <- function(X, cell.info, nu, d = 0.1, m.info = 70, is.scale = TRUE){
     nk <- length(ind.cell.type[[k]])
     Sgm.hat[,,k] <- X[, ind.cell.type[[k]]] %*% t(X[, ind.cell.type[[k]]]) / (nk - 1)
   }
-  
-  
+
+
   #---- Step3 Find the Neighbors of Cell i ----
   #--- Set neighborhood radius r ---
   # according to average density of cells in the whole tissue slice ---
-  
+
   # Area of cell tissue slice
   W <- max(cell.info[,2]) - min(cell.info[,2])
   L <- max(cell.info[,3]) - min(cell.info[,3])
-  
+
   S <- L * W
   # Square neighborhood radius
   r <- sqrt(S * m.info / n) / 2
-  
+
   NeiFind <- function(i){
     ind.i <- as.numeric(cell.info[i,2:3])
     nei.inx <- which(abs(cell.info[,2] - ind.i[1]) < r & abs(cell.info[,3] - ind.i[2]) < r)
@@ -75,13 +78,13 @@ CSSNEst <- function(X, cell.info, nu, d = 0.1, m.info = 70, is.scale = TRUE){
     # Return a matrix with index of neighborhood cells and corresponding cell type.
     return(cbind(cell.index, cell.type.nei))
   }
-  
-  
+
+
   #---- Step4 Estimate Posterior Expectation of Sigma_i ----
   SgmEst <- function(nu.i, i, K, Sgm.hat){
     nei.mat <- data.frame(NeiFind(i))
     colnames(nei.mat) <- c("cell.index", "cell.type")
-    
+
     ni <- nrow(nei.mat)
     if(ni == 0){
       Lambda.i <- (nu.i - G - 1) * Sgm.hat[,,cell.type[[i]]]
@@ -99,59 +102,72 @@ CSSNEst <- function(X, cell.info, nu, d = 0.1, m.info = 70, is.scale = TRUE){
     # posterior mean
     S.i <- X[,i] %*% t(X[,i])
     Sgm.i.hat <- (S.i + Lambda.i) / (nu.i - G)
-    
+
     return(Sgm.i.hat)
   }
-  
-  Sgm.est <- array(NA, dim = c(G, G, n))
-  Corr.est <- array(NA, dim = c(G, G, n))
-  for(i in 1:n){
-    tmp <- SgmEst(nu[i], i, K, Sgm.hat)
-    Corr.est[,,i] <- cov2cor(tmp)
-    Sgm.est[,,i] <- tmp
+
+  if(is.all == TRUE){
+    # Sgm.est <- array(NA, dim = c(G, G, n))
+    Corr.est  <- list()
+    # Corr.est <- array(NA, dim = c(G, G, n))
+    for(i in 1:n){
+      tmp <- SgmEst(nu[i], i, K, Sgm.hat)
+      tmp <- cov2cor(tmp)
+      tmp[abs(tmp) < 0.1] <- 0
+      tmp[tmp != 0] <- 1
+      Corr.est[[i]] <- Matrix(data = tmp, sparse = TRUE)
+    }
+  } else{
+    n2 <- length(indx.cell)
+    Corr.est <- list()
+    for(i in 1:n2){
+      tmp <- SgmEst(nu[indx.cell[i]], indx.cell[i], K, Sgm.hat)
+      tmp <- cov2cor(tmp)
+      tmp[abs(tmp) < 0.1] <- 0
+      tmp[tmp != 0] <- 1
+      Corr.est[[i]] <- Matrix(data = tmp, sparse = TRUE)
+    }
   }
-  
-  #---- Sparse estimation of correlation matrix ----
-  Network <- Corr.est
-  Network[abs(Network) < d] <- 0
-  Network[Network != 0] <- 1
-  return(list("Correlation Matrix" = Corr.est,
-              "Gene Networks" = Network))
+
+  return(Corr.est)
 }
 
 
 CSSNPredict <- function(GN, cell.info, miss.indx, m.info = 70){
-  G <- dim(GN)[1]
+  G <- nrow(GN[[1]])
   miss.num <- nrow(miss.indx)
-  
+
   if(min(miss.indx[,1]) < min(cell.info[,2]) | min(miss.indx[,2]) < min(cell.info[,3]) | max(miss.indx[,1]) > max(cell.info[,2]) | max(miss.indx[,1]) > max(cell.info[,2])){
     stop("Please remove cells out of the range of tissue slice!")
   }
   if(sum(miss.x %in% cell.info[,2]) >0 & sum(miss.y %in% cell.info[,3]) > 0){
     stop("Please remove cells already in the tissue slice!")
   }
-  
+
   W <- max(cell.info[,2]) - min(cell.info[,2])
   L <- max(cell.info[,3]) - min(cell.info[,3])
   S <- L * W
   r <- sqrt(S * m.info / n) / 2
-  
+
   NeiFind <- function(miss.indx.cell){
     nei.indx <- which(abs(cell.info[,2] - miss.indx.cell[1]) < r & abs(cell.info[,3] - miss.indx.cell[2]) < r)
     cell.type.nei <- cell.type[nei.indx]
     return(cbind(nei.indx, cell.type.nei))
   }
-  
-  est.miss <- array(NA, dim = c(G, G, miss.num))
+
+  # est.miss <- array(NA, dim = c(G, G, miss.num))
+  est.miss <- list()
   for(i in 1:miss.num){
     miss.nei <- NeiFind(miss.indx[i,])
-    tmp <- Sparse.Corr[,, miss.nei[,1]]
-    tmp1 <- apply(tmp, 1:2, mean)
-    tmp1[tmp1 < 0.5] <- 0
-    tmp1[tmp1 >= 0.5] <- 1
-    est.miss[,, i] <- tmp1
+    tmp <- matrix(0, G, G)
+    for(j in 1:nrow(miss.nei)){
+      tmp <- tmp + GN[[miss.nei[j,1]]]
+    }
+    tmp <- tmp / miss.num
+    tmp[tmp < 0.5] <- 0
+    tmp[tmp >= 0.5] <- 1
+    est.miss[[i]] <- tmp
   }
-  
+
   return(est.miss)
-  
 }
